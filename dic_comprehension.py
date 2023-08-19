9ec7b21088fb87f4cc984fa9ff9e9ed35da0aab2
Juan Pablo Quispe Molina <juancode29@gmail.com>
2023-08-18 21:27:50 -0500

    Otros ejemplos

--- dic_comprehension.py
+++ dic_comprehension.py
@@ -1,5 +1,6 @@
 #nuevo_diccionario = {clave: valor for elemento in secuencia if condicion}
 #SIN DICT COMPREHENSION
+
 dict = {}
 for i in range(1, 5):
     dict[i] = i*2
@@ -10,7 +11,7 @@ print(dict)
 dict = {i:i*2 for i in range(1, 5)}
 print(dict)
 
-#Generamos población aleatoria para ciudades:
+#ejemplo 01: Generamos población aleatoria para ciudades:
 import random
 paises = ['Per', 'Col', 'Bol']
 poblacion = {}
@@ -23,3 +24,29 @@ print(poblacion)
 
 poblacion = {pais: random.randint(1000, 2000) for pais in paises}
 print(poblacion)
+
+#ejemplo 02:
+
+names = ['Juan', 'Pablo', 'Jose']
+ages = [38, 37, 28]
+
+names_ages = { name: age for name, age in zip(names, ages)}
+print(names_ages)
+
+#Ejemplo 03:
+import random
+paises = ['Per', 'Col', 'Bol']
+poblacion_v1 = { pais: random.randint(1000, 2000) for pais in paises}
+print(poblacion_v1)
+
+poblacion_v2 = {pais: poblacion for pais, poblacion in poblacion_v1.items() if poblacion > 1700}
+print(poblacion_v2)
+
+#Ejemplo 04:
+
+texto = 'Hola, soy Juan Pablo'
+unique = {v: v.upper() for v in texto if v in 'aeiou'}
+print(unique)
+
+frecuencia = {vocal: texto.count(vocal) for vocal in texto if vocal in 'aeiou'}
+print(frecuencia)
