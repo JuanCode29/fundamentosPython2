762f282132a8a25ad3b266061af97ff1746bb304
Juan Pablo Quispe Molina <juancode29@gmail.com>
2023-08-19 14:29:44 -0500

    Ejercios

--- functions.py
+++ functions.py
@@ -45,3 +45,52 @@ def saludar(nombre, mensaje="¡Hola!"):
 
 saludar("Juan")  # Salida: ¡Hola! Juan
 saludar("Ana", "¡Hola de nuevo!")  # Salida: ¡Hola de nuevo! Ana
+
+#EJEMPLOS VARIOS
+
+#1. CALCULAMOS EL ÁREA DE UN RECTANGULO(Usamos PARÁMETROS PREDERTIMINADOS):
+
+def área_rectángulo(base = 4, altura = 10):
+    return base * altura
+
+area = área_rectángulo() #Utilizando valores predeterminados
+print(area)
+
+area_personalizada = área_rectángulo(20, 30)
+print(area_personalizada)
+
+#2. CALCULAMOS LA SUMA DE LOS "n" primeros números naturales:
+
+def suma_numeros(n):
+    suma = 0
+    for i in range(1, n+1):
+        suma = suma + i
+        print(i)
+    print("---")
+    print(suma)
+
+suma_numeros(6)
+
+#2. Factorial de un número:
+
+def factorial_num (n):
+    if n == 1 or n == 0:
+        return 1
+    else:
+        return n*factorial_num(n-1)
+
+resultado = factorial_num(5)
+print(resultado)
+
+#3. Factorial con bucle for
+
+def factorial_num(n):
+    factorial = 1
+    for i in range(1, n + 1):
+        print(i, end=" ")  # Imprimir el valor de i seguido de un espacio en lugar de un salto de línea
+        factorial *= i
+    print()  # Imprimir una línea en blanco para separar la salida
+    return factorial
+
+resultado = factorial_num(5)
+print("Factorial:", resultado)
