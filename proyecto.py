9aa263486d44eb828924737784355f354f38d8dc
Juan Pablo Quispe Molina <juancode29@gmail.com>
2023-09-03 11:40:56 -0500

    Obtener GANADOR

--- proyecto.py
+++ proyecto.py
@@ -1,8 +1,8 @@
 import random
 # LISTA DE OPCIONES DE JUEGO
 opciones = ['Piedra', 'Papel', 'Tijera']
-#OBTENEMOS JUGADA DEL USUARIO CON SU VALIDACIÓN RESPECTIVA
 
+#OBTENEMOS JUGADA DEL USUARIO CON SU VALIDACIÓN RESPECTIVA
 def obtener_opcion_usuario():
     while True:
         opcion = input("Elije Piedra, Papel o Tijera: ").strip().capitalize()
@@ -11,6 +11,19 @@ def obtener_opcion_usuario():
         else:
             print("Opción no válida: Elije Piedra, Papel o Tijera.")
 
+#OBTENEMOS JUGADA DEL COMPUTADOR
 def obtener_opcion_computador():
     return random.choice(opciones)
+
+#OBTENEMOS GANADOR
+
+def obtener_ganador(usuario, computador):
+    if usuario == computador:
+        return 'Empate'
+    elif usuario == 'Papel' and computador == 'Piedra' | usuario == 'Piedra' and computador == 'Tijera' | usuario == 'Tijera' and computador == 'Papel':
+        return 'Usuario'
+    else:
+        return 'Computador'
+
+
 
