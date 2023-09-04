f3f57fba98f431f72701bca0c9f621b08108111f
Juan Pablo Quispe Molina <juancode29@gmail.com>
2023-09-03 21:46:20 -0500

    Ejemplos de función lambda

--- lambda.py
+++ lambda.py
@@ -1,3 +1,17 @@
+# Ejemplo 01:
 cuadrado = lambda x : x**2
 resultado = cuadrado(5)
 print(resultado)
+
+#Ejemplo 02:
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+
+print(mydoubler(11))
+
+#Ejemplo 03:
+full_name = lambda name, last_name: f'Mi nombres es: {name.title()} {last_name.title()}'
+
+print(full_name('Juan', 'Quispe Molina'))
