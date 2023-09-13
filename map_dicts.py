b5003cddd23ad6856c11a479ebcb703490401324
Juan Pablo Quispe Molina <juancode29@gmail.com>
2023-09-12 19:35:51 -0500

    Ejemplo final

--- map_dicts.py
+++ map_dicts.py
@@ -10,3 +10,26 @@ def calcular_IGV(producto):
 
 productos_final = list(map(calcular_IGV, productos))
 print(productos_final)
+
+
+#EJEMPLO 02 - LISTAS
+
+numeros = [1, 2, 3, 4, 5, 6]
+calcular_cuadrado = lambda n: n*n
+print(list(map(calcular_cuadrado,numeros)))
+
+#EJEMPLO 03 - DICCIONARIOS
+
+rectangulos = [
+    {'longitud': 5, 'ancho': 4},
+    {'longitud': 3, 'ancho': 6},
+    {'longitud': 7, 'ancho': 2}
+]
+
+def calcular_area(rectangulo):
+    rectangulo['area'] = rectangulo['longitud'] * rectangulo['ancho']
+    return rectangulo
+
+print(list(map(calcular_area, rectangulos)))
+print(list(map(lambda rectangulo: rectangulo['longitud'], rectangulos)))
+print(list(map(lambda rectangulo: rectangulo['ancho'], rectangulos)))
