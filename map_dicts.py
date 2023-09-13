c4d5cab7890f3b805be1caf10076ff5f77dca05e
Juan Pablo Quispe Molina <juancode29@gmail.com>
2023-09-12 22:55:42 -0500

    Ejemplos y observaciones

--- map_dicts.py
+++ map_dicts.py
@@ -31,5 +31,35 @@ def calcular_area(rectangulo):
     return rectangulo
 
 print(list(map(calcular_area, rectangulos)))
+
+# AL APLICAR la función calcular_area dentro de map(); modifica los diccionarios originales en la lista rectángulos.
+
+print(rectangulos)
+
+#Forma de imprimir solo las claves de los diccionarios.
 print(list(map(lambda rectangulo: rectangulo['longitud'], rectangulos)))
+
+#Forma de imprimir solo los valores de las palabras clave de los diccionarios.
 print(list(map(lambda rectangulo: rectangulo['ancho'], rectangulos)))
+
+
+#EJEMPLO 04: Para evitar cambiar el diccionario original, realizamos lo siguiente
+
+rectangulos2 = [
+    {'longitud': 5, 'ancho': 4},
+    {'longitud': 3, 'ancho': 6},
+    {'longitud': 7, 'ancho': 2}
+]
+
+# Definimos una función para calcular el área y crear un nuevo diccionario
+def calcular_area2(rectangulo2):
+    area = rectangulo2['longitud'] * rectangulo2['ancho']
+    return {**rectangulo2, 'area': area}#** permite crear un nuevo diccionario y agregarle una nueva clave
+
+#Utilizar una comprensión de lista para crear nuevos diccionarios con áreas
+
+rectangulos_con_areas = [calcular_area2(dimension) for dimension in rectangulos2]
+
+print(rectangulos_con_areas)
+
+# Solo que en este caso ya no se utilizó map
