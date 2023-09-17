8f1d71f92fa8c4f9610ace53328cde126f0508cc
Juan Pablo Quispe Molina <juancode29@gmail.com>
2023-09-16 19:07:36 -0500

    Se a creado una variable, una función y un diccionario

---
+++ App/utils.py
@@ -0,0 +1,17 @@
+a = 'Hello'
+
+def greeting(name):
+    print("Hello, " + name)
+
+person1 = [
+    {
+    "name": "John",
+    "age": 36,
+    "country": "Norway"
+    },
+    {
+    "name": "Jose",
+    "age": 36,
+    "country": "Norway"
+    }
+]
