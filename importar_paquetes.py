3c26d000534c7fd6801bd608dd203603d4a75368
Juan Pablo Quispe Molina <juancode29@gmail.com>
2023-09-18 00:22:10 -0500

    Segunda forma de importar un paquete

--- importar_paquetes.py
+++ importar_paquetes.py
@@ -2,3 +2,7 @@ from pqt.modulo_1 import func1, func2
 
 print(func1())
 print(func2())
+
+from pqt import modulo_2 #De esta forma si realizas una actualizacion en el módulo después de haberlo importado vs code lo reconoce y lo ejecuta
+print(modulo_2.func3())
+print(modulo_2.func4())
