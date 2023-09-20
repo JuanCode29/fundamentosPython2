0c5e0d8665529b9cd047b20d411ded514123cffa
Juan Pablo Quispe Molina <juancode29@gmail.com>
2023-09-19 19:03:54 -0500

    Iterables

---
+++ interables.py
@@ -0,0 +1,10 @@
+for i in range(1, 10):
+    print(i)
+
+my_iterable = iter(range(1, 5))
+print(my_iterable)
+print(next(my_iterable))
+print(next(my_iterable))
+print(next(my_iterable))
+print(next(my_iterable))
+print(next(my_iterable))
