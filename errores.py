6f2c53eb9f5973c04c1acade9b877df04dd70737
Juan Pablo Quispe Molina <juancode29@gmail.com>
2023-09-19 22:58:19 -0500

    Ultima actualización

--- errores.py
+++ errores.py
@@ -10,5 +10,11 @@
 # Uso de assert para verificaciones:se utiliza para verificar que una condición sea verdadera. Si la condición es falsa, se lanza una excepción AssertionError.
 print('Hola')
 suma = lambda x, y: x + y
-assert suma(4, 6) == 9, "La suma es incorrecta" # AssertionError: La suma es incorrecta
+assert suma(4, 9) == 9, "La suma es incorrecta" # AssertionError: La suma es incorrecta.
+#Si la condición es falta la ejecución del programa termina.
 print('Hola2')
+
+#lanzar tus propias excepciones usando la instrucción raise:
+edad = 15
+if edad < 18:
+    raise ValueError ('La edad no puede ser menor de 18 años')
