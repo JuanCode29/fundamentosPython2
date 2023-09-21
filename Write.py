c909d3770153e7d53acfb128fe57543feb4e2fd2
Juan Pablo Quispe Molina <juancode29@gmail.com>
2023-09-20 21:41:38 -0500

    Creamos un nuevo archivo y escribimos en el. Ejemplos utilzando los parámetros x, a, w

--- Write.py
+++ Write.py
@@ -1,2 +1,15 @@
-#CREAMOS UN ARCHIVO
-archivo = open('miArchivo2.txt', 'x')
+#CREAMOS y ESCRIBIMOS UN ARCHIVO
+#'x' (Modo de creación exclusiva): Este modo se utiliza para crear un archivo nuevo y, si ya existe un archivo con el mismo nombre, se generará un error (FileExistsError). Es útil cuando deseas asegurarte de que el archivo no exista previamente antes de crearlo.#
+
+with open('archivo_nuevo.txt', 'x') as archivo:
+    archivo.write('Este es un nuevo archivo.')
+
+#'a' (Modo de agregado): En este modo, el archivo se abre para escritura, pero si el archivo ya existe, el nuevo contenido se agrega al final del archivo en lugar de sobrescribirlo. Si el nuevo texto queremos agregarlo a la siguiente línea utilizamos '\n'#
+
+with open('archivo_nuevo.txt', 'a') as archivo:
+    archivo.write("\nEste contenido se agrega al final del archivo.")
+
+#'w' (Modo de escritura): Este modo se utiliza para abrir un archivo en modo escritura. Si el archivo ya existe, su contenido se sobrescribirá. Si no existe, se creará un archivo nuevo.#
+
+with open('miArchivo2.txt', 'w') as archivo:
+    archivo.write("Este contenido sobrescribirá el contenido anterior.")
